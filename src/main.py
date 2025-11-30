aa04fbe0110e7256b5c1d4621a4cf0eb01aaf107
مهدی فرهانی <m.farhani@entekhabgroup.ir>
2025-11-30 19:19:13 +0330

    Add otsu

--- src/main.py
+++ src/main.py
@@ -26,6 +26,42 @@ def thresholding_binary(gray:np.array, threshold:int=128):
 
     binary = np.where(gray >= threshold, 255, 0)
     return binary.astype(np.uint8)
+def otsu_thresholding(gray:np.array):
+    histogram, _ = np.histogram(gray, bins=256, range=(0, 256))
+    total_pixels = gray.size
+
+    current_max = 0
+    threshold = 0
+    sum_total = np.dot(np.arange(256), histogram)
+    sum_background = 0
+    weight_background = 0
+
+    for t in range(256):
+        weight_background += histogram[t]
+        if weight_background == 0:
+            continue
+
+        weight_foreground = total_pixels - weight_background
+        if weight_foreground == 0:
+            break
+
+        sum_background += t * histogram[t]
+
+        mean_background = sum_background / weight_background
+        mean_foreground = (sum_total - sum_background) / weight_foreground
+
+        between_class_variance = weight_background * weight_foreground * (mean_background - mean_foreground) ** 2
+
+        if between_class_variance > current_max:
+            current_max = between_class_variance
+            threshold = t
+
+    return threshold
+
+def apply_otsu_thresholding(gray:np.array):
+    threshold = otsu_thresholding(gray)
+    binary = thresholding_binary(gray, threshold)
+    return binary, threshold
 
 if __name__ == "__main__":
     (originalImage,rgb)=loadImage("../samples/22013.jpg")
@@ -35,10 +71,18 @@ if __name__ == "__main__":
 
     binary = thresholding_binary(gray, threshold=threshold)
 
-    plt.figure(figsize=(15,4))
-    plt.subplot(1, 4, 1); plt.title("Original"); plt.imshow(rgb.astype(np.uint8)); plt.axis('off')
-    plt.subplot(1, 4, 2); plt.title("Grayscale"); plt.imshow(gray, cmap='gray'); plt.axis('off')
-    plt.subplot(1, 4, 3); plt.title("Negative"); plt.imshow(negative, cmap='gray'); plt.axis('off')
-    plt.subplot(1, 4, 4); plt.title(f"Binary (t:{threshold})"); plt.imshow(negative, cmap='gray'); plt.axis('off')
+    binary_otsu, otsu_threshold=apply_otsu_thresholding(gray)
+
+    print("Otsu Threshold =", otsu_threshold)
+
+
+    plt.figure(figsize=(18,8))
+    plt.subplot(2, 4, 1); plt.title("Original"); plt.imshow(rgb.astype(np.uint8)); plt.axis('off')
+    plt.subplot(2, 4, 2); plt.title("Grayscale"); plt.imshow(gray, cmap='gray'); plt.axis('off')
+    plt.subplot(2, 4, 3); plt.title("Negative"); plt.imshow(negative, cmap='gray'); plt.axis('off')
+    plt.subplot(2, 4, 4); plt.title(f"Binary (t:{threshold})"); plt.imshow(binary, cmap='gray'); plt.axis('off')    
+    plt.subplot(2, 4, 5); plt.title("Histogram"); plt.hist(gray.flatten(), bins=256)
+    plt.subplot(2, 4, 8); plt.title(f"Otsu Binary (t:{otsu_threshold})"); plt.imshow(binary_otsu, cmap='gray'); plt.axis('off')    
+
 
     plt.show()
